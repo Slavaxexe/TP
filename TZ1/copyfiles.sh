82d6ad8bbe4c4fd20a67880ed03867f339ff677f
Slavaxexe <74611428+Slavaxexe@users.noreply.github.com>
2024-04-29 02:02:27 +0300

    Update copyfiles.sh
    пометки

--- TZ1/copyfiles.sh
+++ TZ1/copyfiles.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-
+#получение директорий
 input_dir=$1
 output_dir=$2
 
 
 find "$input_dir" -type f | while read file; do
+    #получение название файла без пути, его расширения и имени
     basefile=$(basename "$file")
     ext="${basefile##*.}"
     name="${basefile%.*}"
-    
+    #создание неповторяющегося названия файла
     new_file="$output_dir/$basefile"
     count=1
-    while [ -f "$new_file" ]; do
+    while [[ -f "$new_file" ]]; do
         new_file="$output_dir/${name}-$count.$ext"
         ((count++))
     done
-
+    #копирование файла
     cp "$file" "$new_file"
 done
 
