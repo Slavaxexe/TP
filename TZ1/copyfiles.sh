61d697a34ebf80f213af9a2879682bd5a16e4ba5
Slavaxexe <74611428+Slavaxexe@users.noreply.github.com>
2024-05-02 22:40:45 +0300

    Update copyfiles.sh

--- TZ1/copyfiles.sh
+++ TZ1/copyfiles.sh
@@ -4,8 +4,8 @@
 input_dir=$1
 output_dir=$2
 
-
-find "$input_dir" -type f | while read file; do
+# Используем опцию -iname с шаблоном '*', чтобы найти все файлы, включая скрытые
+find "$input_dir" -type f -iname '*' | while read file; do
     #получение название файла без пути, его расширения и имени
     basefile=$(basename "$file")
     ext="${basefile##*.}"
@@ -20,4 +20,3 @@ find "$input_dir" -type f | while read file; do
     #копирование файла
     cp "$file" "$new_file"
 done
-
